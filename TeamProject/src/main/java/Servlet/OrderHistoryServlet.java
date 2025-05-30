fcc424812299ffb2b0e878e7824e2e5c8778f3f7
issohbog <qkrehgus2312@naver.com>
2025-05-30 17:15:07 +0900

    모든 Servlet 에서 userNo 세션에서 받아오기 기능 구현 완료!

--- TeamProject/src/main/java/Servlet/OrderHistoryServlet.java
+++ TeamProject/src/main/java/Servlet/OrderHistoryServlet.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 import DAO.OrderDAO;
 import DTO.Order;
+import DTO.User;
 import Service.OrderService;
 import Service.OrderServiceImpl;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 @WebServlet("/order/history")
@@ -22,15 +23,19 @@ public class OrderHistoryServlet extends HttpServlet {
 	private OrderService orderService = new OrderServiceImpl(new OrderDAO());
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		HttpSession session = request.getSession();
-//		Integer userNo = (Integer) session.getAttribute("loginUserNo");	
-//		if (userNo == null) {
-//			response.sendRedirect(request.getContextPath() + "/login.jsp");
-//			return;  
-//		}
 		
+		// userNo를 세션에서 받아오기
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");	
 		
-		int userNo = 5;
+		if (loginUser == null) {
+			response.sendRedirect(request.getContextPath() + "/login.jsp");
+			return;  
+		}
+		
+		Integer userNo = loginUser.getUserNo();
+		
+
 		List<Order> orders = orderService.getOrderByUserNo(userNo); 
 		request.setAttribute("orders", orders);
 		System.out.println(orders);
